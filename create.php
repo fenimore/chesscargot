95d4ade2d79cd6636829f3bcaba638d5daa5581a
polypmer <exorable.ludos@gmail.com>
2016-11-21 14:48:19 -0500

    Fix critical security issue

--- create.php
+++ create.php
@@ -1,53 +1,59 @@
 <?php
 
-	require 'database.php';
+    require 'database.php';
 
-	if ( !empty($_POST)) {
-		// keep track validation errors
-		$infoError = null;
-		$whiteError = null;
-		$blackError = null;
-		$pgnError = null;
-		$commentsError = null;
+    if ( !empty($_POST)) {
+        // keep track validation errors
+        $infoError = null;
+        $whiteError = null;
+        $blackError = null;
+        $pgnError = null;
+        $commentsError = null;
 
-		// keep track post values
-		$info = $_POST['info'];
-		$white = $_POST['white'];
-		$black = $_POST['black'];
-		$pgn = $_POST['pgn'];
-		$comments = $_POST['comments'];
-		$mydate = getdate();
+        // keep track post values
+        $info = $_POST['info'];
+        $white = $_POST['white'];
+        $black = $_POST['black'];
+        $pgn = $_POST['pgn'];
+        $comments = $_POST['comments'];
+        $mydate = getdate();
     $date = $mydate[year] . '.' . $mydate[mon] . '.' .  $mydate[mday] ;
     $fen = 'rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1';
     $result = '*';
-    
-		// validate input
-		$valid = true;
-		if (empty($info)) {
-			$infoError = 'Please enter date, or other information';
-			$valid = false;
-		}
 
-		if (empty($white)) {
-			$whiteError = 'who is white?';
-			$valid = false;
-		}
+        // validate input
+        $valid = true;
+        if (empty($info)) {
+            $infoError = 'Please enter date, or other information';
+            $valid = false;
+        }
 
-		if (empty($black)) {
-			$blackError = 'who is black?';
-			$valid = false;
-		}
-		// insert data
-		if ($valid) {
-			$pdo = Database::connect();
-			$pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-			$sql = "INSERT INTO chessgames (info,white,black,pgn,comments,date,fen,result) values(?, ?, ?, ?, ?, ?, ?, ?)";
-			$q = $pdo->prepare($sql);
-			$q->execute(array($info,$white,$black,$pgn,$comments,$date,$fen,$result));
-			Database::disconnect();
-			header("Location:index.php#recent");
-		}
-	}
+        if (empty($white)) {
+            $whiteError = 'who is white?';
+            $valid = false;
+        }
+
+        if (empty($black)) {
+            $blackError = 'who is black?';
+            $valid = false;
+        }
+        // insert data
+        if ($valid) {
+            $pdo = Database::connect();
+            $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+            $sql = "INSERT INTO chessgames (info,white,black,pgn,comments,date,fen,result) values(?, ?, ?, ?, ?, ?, ?, ?)";
+            $q = $pdo->prepare($sql);
+            $q->execute(array(htmlspecialchars($info),
+                              htmlspecialchars($white),htmlspecialchars($black),
+                              htmlspecialchars($pgn),
+                              htmlspecialchars($comments),
+                              htmlspecialchars($date),
+                              htmlspecialchars($fen),
+                              htmlspecialchars($result)));
+            Database::disconnect();
+            header("Location:index.php#recent");
+        }
+    }
 ?>
 <!DOCTYPE html>
 <html lang="fr">
@@ -61,7 +67,7 @@
     <link rel="icon" href="img/favicon.ico">
 
     <title>Chesscargot - Nouveau</title>
-    
+
     <!-- Bootstrap core CSS -->
     <link href="css/bootstrap.min.css" rel="stylesheet">
     <!-- Font Awesome core CSS -->
@@ -77,4 +83,3 @@
 Woops. Il faut que vous remplissez les champs 'info', 'blanc', et 'noir' avant continuer.<br>
 <a href="index.php#lancer">Retour</a></div></body>
 </html>
-
